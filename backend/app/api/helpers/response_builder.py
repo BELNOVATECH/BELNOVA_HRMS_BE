65a8c7cf72179cf545967377a7dbb3f33ed1bec9
PROJECTSRM1 <projects@rm1codershub.com>
2025-12-09 15:27:07 +0530

    Delete backend directory

--- backend/app/api/helpers/response_builder.py
+++
@@ -1,8 +0,0 @@
-def response(success=True, message="", data=None, error=None, status_code=200):
-    return {
-        "success": success,
-        "message": message,
-        "data": data,
-        "error": error,
-        "status_code": status_code
-    }
