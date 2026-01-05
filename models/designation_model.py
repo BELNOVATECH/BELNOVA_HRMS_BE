9011e3f4a1c52ea36723dfa7fbd5072ca9f1390b
RajashekarRM1 <rajashekar.maddi@rm1codershub.com>
2026-01-05 13:02:32 +0530

    added designation_name

--- models/designation_model.py
+++ models/designation_model.py
@@ -1,5 +1,7 @@
+
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from core.database import Base
+
 
 class Designation(Base):
     __tablename__ = "master_designation"
@@ -7,10 +9,6 @@ class Designation(Base):
     id = Column(Integer, primary_key=True, index=True)
     designation_name = Column(String(100), nullable=False)
 
-    dept_id = Column(
-        Integer,
-        ForeignKey("master_department.id"),
-        nullable=False
-    )
-
+    dept_id = Column(Integer, ForeignKey("master_department.id"))
     is_active = Column(Boolean, default=True)
+
